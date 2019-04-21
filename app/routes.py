3eabb3f21d33f274aab52464b5f3c357900587f2
Eddie Atkinson <22487668@student.uwa.edu.au>
2019-04-21 21:32:20 +0800

    sets up base.html prototype and further changes project structure

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from app import app
+from flask import render_template
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return("hello world")
+    return(render_template("index.html", title = "Home"))
+    
