706e69c06399a4a0219a4e183a19400dd72d6314
Eddie Atkinson <22487668@student.uwa.edu.au>
2019-04-22 11:33:53 +0800

    changes routes.py to flash message indicating a request has been received

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect
 from app.forms import LoginForm
 from flask_login import current_user, login_user
+
 
 
 @app.route("/")
@@ -9,7 +10,10 @@ from flask_login import current_user, login_user
 def index():
     return(render_template("index.html", title = "Home"))
 
-@app.route("/login")
+@app.route("/login", methods = ["POST", "GET"])
 def login():
     form = LoginForm()
+    if(form.validate_on_submit()):
+        flash("Login requested for {}".format(form.username.data))
+        return(redirect("/index"))
     return(render_template("login.html", title =  "Sign In", form = form))
