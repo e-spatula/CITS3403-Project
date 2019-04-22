6fca1e8b7fd836c1ac823be50460e56678993b1c
Eddie Atkinson <22487668@student.uwa.edu.au>
2019-04-22 10:46:33 +0800

    creates basic login form

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,15 @@
 from app import app
 from flask import render_template
+from app.forms import LoginForm
+from flask_login import current_user, login_user
+
 
 @app.route("/")
 @app.route("/index")
 def index():
     return(render_template("index.html", title = "Home"))
-    
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return(render_template("login.html", title =  "Sign In", form = form))
