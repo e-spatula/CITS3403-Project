1b030fddfefbff1425c79888cee19359bd3ce5c3
Eddie Atkinson <22487668@student.uwa.edu.au>
2019-05-08 10:01:28 +0800

    adds registration

--- app/forms.py
+++ app/forms.py
@@ -11,7 +11,7 @@ class LoginForm(FlaskForm):
 class RegistrationForm(FlaskForm):
     username = StringField("Username", validators = [DataRequired()])
     email = StringField("Email", validators = [DataRequired(), Email()])
-    password = StringField("Password", validators = [DataRequired()])
+    password = PasswordField("Password", validators = [DataRequired()])
     password2 = PasswordField(
         "Repeat Password", validators = [DataRequired(), EqualTo("password")]
     )
@@ -26,4 +26,3 @@ class RegistrationForm(FlaskForm):
         email = User.query.filter_by(email = email.data).first()
         if(email is not None):
             raise ValidationError("Email already registered.")
-    
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
-from app import app
+from app import app, db
 from flask import render_template, flash, redirect, url_for, request
-from app.forms import LoginForm
+from app.forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
 from werkzeug.urls import url_parse
@@ -40,3 +40,17 @@ def logout():
 def profile():
     name = current_user.get_name()
     return(render_template("profile.html", name = name))
+
+@app.route("/register", methods = ["POST", "GET"])
+def register():
+    if(current_user.is_authenticated):
+        return(redirect(url_for("index")))
+    form = RegistrationForm()
+    if(form.validate_on_submit()):
+        user = User(username = form.username.data, email = form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash("Congratulations you have now joined")
+        return(redirect(url_for("login")))
+    return(render_template("register.html", title = "Register", form = form))
