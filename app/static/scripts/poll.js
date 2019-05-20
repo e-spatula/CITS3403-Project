b0c3bc8b5d2d2d8ea710d891c9e9636cb9e6273c
Eddie Atkinson <22487668@student.uwa.edu.au>
2019-05-20 21:11:14 +0800

    removes variable in poll.js

--- app/static/scripts/poll.js
+++ app/static/scripts/poll.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    let $options = $("#options").children("label").each(function() {
+    $("#options").children("label").each(function() {
         this.textContent = moment(this.textContent).format("dddd, MMMM Do YYYY, h:mm:ss a");
     });
 });
