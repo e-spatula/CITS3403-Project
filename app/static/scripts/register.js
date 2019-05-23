db609f77de2090413b483ba5b9a2c132c27c151d
Eddie Atkinson <22487668@student.uwa.edu.au>
2019-05-23 13:00:39 +0800

    adds javascript form validation for registration

--- app/static/scripts/register.js
+++ app/static/scripts/register.js
@@ -20,8 +20,36 @@ $(document).ready(function() {
             displayText("Email can't be blank", "error");
         } else {
             if(isValidEmail(this.value)) {
-
+                clearText();
+            } else {
+                displayText("Invalid email address", "error");
             }
+        }
+    });
+
+    $("#password").blur(function() {
+        let $password2 = $("#password2").val();
+
+        if(this.value.length === 0) {
+            displayText("Password can't be blank", "error");
+        }
+        else if($password2.length > 0 && this.value != $password2) {
+            displayText("Passwords must match", "error");
+        } else {
+            clearText();
+        }
+    });
+
+    $("#password2").blur(function() {
+        let $password = $("#password").val();
+
+        if(this.value.length === 0) {
+            displayText("Confirm password can't be blank", "error");
+        }
+        else if(this.value != $password) {
+            displayText("Passwords must match", "error");
+        } else {
+            clearText();
         }
     });
 });
@@ -31,6 +59,7 @@ if an email is valid is to attempt to send an email to it. This is done on the b
 link. The check below is more to correct for incompetence than malevolence.
 */
 function isValidEmail(email) {
-    
+    let re = /.+@.+\..+/g;
+    return(re.test(String(email).toLowerCase()));
 }
 
