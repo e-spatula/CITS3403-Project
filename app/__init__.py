e3981ce30d2bc2eff2cdf728700f4607d821cdbf
Eddie Atkinson <22487668@student.uwa.edu.au>
2019-05-23 12:24:48 +0800

    adds dotenv file for mail password

--- app/__init__.py
+++ app/__init__.py
@@ -5,12 +5,15 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_moment import Moment
 from flask_mail import Mail, Message
+from dotenv import load_dotenv
 import os
 
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+load_dotenv(override = True)
 
 dirname = os.path.dirname(os.path.abspath(__file__))
 USER_UPLOAD_FOLDER = dirname + "/static/user-images/"
@@ -23,7 +26,7 @@ app.config ["MAIL_PORT"] = 587
 app.config ["MAIL_USE_TLS"] =  True 
 app.config ["MAIL_USERNAME"] = "flaskpolly@gmail.com"
 app.config ["MAIL_DEFAULT_SENDER"] = "flaskpolly@gmail.com"
-app.config ["MAIL_PASSWORD"] = os.environ.get("MAIL_PASSWORD")
+app.config ["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD")
 
 
 mail = Mail(app)
--- app/static/scripts/register.js
+++ app/static/scripts/register.js
@@ -26,6 +26,10 @@ $(document).ready(function() {
     });
 });
 
+/* Though it would be possible to use a complex email matching regex pattern, I as the best way to check
+if an email is valid is to attempt to send an email to it. This is done on the backend via an email confirmation
+link. The check below is more to correct for incompetence than malevolence.
+*/
 function isValidEmail(email) {
     
 }
