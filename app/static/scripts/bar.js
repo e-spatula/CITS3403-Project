2d04cfd9b565930abbd93414b83c2cec569f6ec6
Eddie Atkinson <22487668@student.uwa.edu.au>
2019-05-24 14:01:21 +0800

    adds working graph to poll results page

--- app/static/scripts/bar.js
+++ app/static/scripts/bar.js
@@ -14,21 +14,46 @@ $(document).ready(function() {
             }
         });
 
+        $(".graph").append("<span class='graphRowLabel'>100</span>");
+        $(".graph").append("<span class='graphRowLabel'>90</span>");
+        $(".graph").append("<span class='graphRowLabel'>80</span>");
+        $(".graph").append("<span class='graphRowLabel'>70</span>");
+        $(".graph").append("<span class='graphRowLabel'>60</span>");
+        $(".graph").append("<span class='graphRowLabel'>50</span>");
+        $(".graph").append("<span class='graphRowLabel'>40</span>");
+        $(".graph").append("<span class='graphRowLabel'>30</span>");
+        $(".graph").append("<span class='graphRowLabel'>20</span>");
+        $(".graph").append("<span class='graphRowLabel'>10</span>");
+        console.log("working");
+
         Object.keys(days).forEach(function(day) {
             let adjustedCount = (days[day] / count);
             adjustedCount = adjustedCount * 100;
             let barDiv = document.createElement("div");
+            
+            barDiv.className += "graphBar ";
+            barDiv.className += "h" + adjustedCount;
+            
+        
+            document.getElementsByClassName("graph")[0].appendChild(barDiv);
+        });
+
+        $(".graph").append("<span><sup>Y </sup>&frasl;<sub> X</sub></span>");
+
+
+        Object.keys(days).forEach(function(day) {
+            let adjustedCount = (days[day] / count);
+            adjustedCount = adjustedCount * 100;
+    
             let labelSpan = document.createElement("span");
 
             labelSpan.className += "graphColumnLabel";
             labelSpan.innerHTML = day;
 
-            barDiv.className += "graphBar ";
-            barDiv.className += "h" + adjustedCount;
             
-            document.getElementById("labels").appendChild(labelSpan)
-            document.getElementById("bars").appendChild(barDiv);
-            console.log(adjustedCount);
+            document.getElementsByClassName("graph")[0].appendChild(labelSpan);
         });
+
+
     });
 });
