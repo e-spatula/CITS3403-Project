9eb15cb293f34368efa5daff3ae0680aeb917de6
Eddie Atkinson <22487668@student.uwa.edu.au>
2019-05-26 11:03:32 +0800

    finishes back end documentation

--- app/email.py
+++ app/email.py
@@ -4,7 +4,7 @@ from app import mail, app
 
 
 """
-Function for sending
+Function for sending emails to users.
 """
 def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender = sender, recipients = recipients)
@@ -13,12 +13,18 @@ def send_email(subject, sender, recipients, text_body, html_body):
     mail.send(msg)
 
 
+"""
+Function for sending password reset links. 
+"""
 def send_password_reset_email(user):
     token = user.get_reset_password_token()
     send_email("Password reset token", sender = app.config["MAIL_DEFAULT_SENDER"],
      recipients = [user.email], text_body = render_template("email/reset_password.txt", user = user,
      token = token), html_body = render_template("email/reset_password.html", user = user, token = token))
-    
+
+"""
+Function for sending confirmation emails.
+"""
 def send_confirmation_email(user):
      token = user.generate_confirmation_token()
      send_email("Email address confirmation email", sender = app.config["MAIL_DEFAULT_SENDER"],
