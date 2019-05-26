eb72e71db70e2585ccf893019784eb8f02aadc5f
Eddie Atkinson <22487668@student.uwa.edu.au>
2019-05-26 11:02:22 +0800

    fixes text box

--- app/email.py
+++ app/email.py
@@ -2,6 +2,10 @@ from flask_mail import Message
 from flask import render_template
 from app import mail, app
 
+
+"""
+Function for sending
+"""
 def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender = sender, recipients = recipients)
     msg.body = text_body
